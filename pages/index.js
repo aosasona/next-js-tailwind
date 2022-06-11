3e6cd8a97378b61a9cf5561a7dd1f0630c60a9f6
Ayodeji <drealayodeji@icloud.com>
2022-06-11 14:52:23 +0100

    Update: tailwind with postcss

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import Meta from "@/defaults/Meta";
 
 export default function Home() {
   return (
-    <div className="bg-neutral-300">
-      <Meta />
-      Hello World
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <Meta title="Home" desc="This is a description" />
+      Hello World!
+      <h1 className="text-6xl font-bold my-5">Tailwind is working</h1>
     </div>
   );
 }
